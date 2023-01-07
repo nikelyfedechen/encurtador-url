443da3a61071ff59e874a470a54edb860e963ae9
Nikely Fedechen <nikelyfedechen@Nikelys-MacBook-Air.local>
2023-01-07 14:37:05 -0300

    :construction: Adicionando rota e gerador randomico

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Encurtaí' });
 });
 
+function generateCode() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+router.post('/new', (req, res, next) => {
+  const url = req.body.url;
+  const code = generateCode();
+  res.send('http://localhost:3000/' + code);
+})
+
 module.exports = router;
